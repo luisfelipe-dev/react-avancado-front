455a7286fce41c89411977aa618dcf6c93202dc9
Luís Felipe <degaus@live.com>
2020-07-13 00:17:23 -0300

    Criação de componente e testes via styled component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as Styled from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Styled.Wrapper>
+    <Styled.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <Styled.Title>React Avançado</Styled.Title>
+    <Styled.Description>
+      Typescript, ReacJS, NextJS e Styled Components
+    </Styled.Description>
+    <Styled.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente com a tela com código"
+    />
+  </Styled.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('Corres Corretas', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
 })
