5e49dc518f3561c3327262b3ac27f6cbfeb3d0dc
Luís Felipe <degaus@live.com>
2020-07-14 00:11:03 -0300

    Finalizando configuração do storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as Styled from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReacJS, NextJS e Styled Components'
+}) => (
   <Styled.Wrapper>
     <Styled.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <Styled.Title>React Avançado</Styled.Title>
-    <Styled.Description>
-      Typescript, ReacJS, NextJS e Styled Components
-    </Styled.Description>
+    <Styled.Title>{title}</Styled.Title>
+    <Styled.Description>{description}</Styled.Description>
     <Styled.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente com a tela com código"
